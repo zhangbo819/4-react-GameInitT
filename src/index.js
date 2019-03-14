39fe798d0d181af6de42ba9e89e64bc4795a2fbb
zzb <https://github.com/zhangbo819/-.git>
2019-03-15 00:45:14 +0800

    add Resource & nedd babel?

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('index')
+// import './Resource/react';
+// import './Resource/react-dom';
+import './Resource/browser.min.js'
+import './Resource/react.js';  
+import './Resource/react-dom.js'; 
+
+console.log('init ********')
